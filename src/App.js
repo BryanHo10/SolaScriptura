468ec5132c54df8039bf1502e8fcee7f9c0668b9
BryanHo10 <bryankho1010@gmail.com>
2021-06-24 23:39:53 -0700

    Protect unathenticated routing; Uodate yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { isEmpty } from 'lodash';
-import logo from './tree.png';
 import Dashboard from './components/Dashboard';
 import {
 	BrowserRouter as Router,
 	Switch,
 	Route,
-	Link,
 	useLocation,
 } from 'react-router-dom';
-import { Container, Card, Menu } from 'semantic-ui-react';
 import ReadingView from './components/Reading';
-import { STORAGE_META } from './constants/keys';
 import Login from './components/Login/Login';
 import { useUserContext } from './contexts/WithUserProfile';
 
@@ -19,22 +15,21 @@ const HomePage = (props) => {
 	const [bibleURL, setBibleURL] = useState('/bible');
 	const [user] = useUserContext();
 
-	useEffect(() => {
-		const storedBookId = localStorage.getItem(STORAGE_META.LATEST_BOOK_ID);
-		const storedChapterId = localStorage.getItem(STORAGE_META.LATEST_CHAP_ID);
-		if (!isEmpty(storedBookId) && !isEmpty(storedChapterId)) {
-			setBibleURL(`/bible/${storedBookId}/${storedChapterId}`);
-		}
-	}, []);
-
 	if (isEmpty(user)) {
 		return <Login />;
 	}
 
 	return (
-		<Container>
-			<Dashboard />
-		</Container>
+		<>
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Dashboard} />
+					<Route exact path="/bible" component={ReadingView} />
+					<Route path="/bible/:bookId/:chapterId" component={ReadingView} />
+					<Route path="*" component={ErrorPage} />
+				</Switch>
+			</Router>
+		</>
 	);
 };
 const ErrorPage = () => {
@@ -49,17 +44,5 @@ const ErrorPage = () => {
 		</div>
 	);
 };
-function App() {
-	return (
-		<Router>
-			<Switch>
-				<Route exact path="/" component={HomePage} />
-				<Route exact path="/bible" component={ReadingView} />
-				<Route path="/bible/:bookId/:chapterId" component={ReadingView} />
-				<Route path="*" component={ErrorPage} />
-			</Switch>
-		</Router>
-	);
-}
 
-export default App;
+export default HomePage;
